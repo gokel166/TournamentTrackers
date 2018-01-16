3a99d730e137fb443785d9a43cbfa742ba234cc3
George Okello <okellogeorge40@yahoo.com>
2018-01-16 00:00:53 -0600

    Add new models

---
+++ StatTrackerLib/TournamentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatTrackerLib
+{
+    public class TournamentModel
+    {
+        public string TournamentName { get; set; }
+        public decimal EntryFee { get; set; }
+        public List<TeamModel> EnteredTeams { get; set; } = new List<TeamModel>();
+        public List<PrizedModel> Prizes { get; set; } = new List<PrizedModel>();
+        public List<List<MatchupModel>> Rounds { get; set; } = new List<List<MatchupModel>>();
+    }
+}
