6e020c529c9367d2696d1302ec9ae21d1dc1acf6
George Okello <okellogeorge40@yahoo.com>
2018-01-17 04:54:25 -0600

    Start Interface for Prize Model

---
+++ StatTrackerLib/GlobalConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatTrackerLib
+{
+    public class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; }
+
+        public static void InitializeConnection(bool database, bool textFiles)
+        {
+            if (database)
+            {
+                // Create the SQL Connection
+            }
+
+            if (textFiles)
+            {
+                // Create the Text Connection
+            }
+        }
+    }
+}
