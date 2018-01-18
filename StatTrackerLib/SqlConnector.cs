87e1b9c27f2b73892bb04de3799d1d2f770becc5
George Okello <okellogeorge40@yahoo.com>
2018-01-18 12:32:10 -0600

    Apply last UI forms and SQL interface

--- StatTrackerLib/SqlConnector.cs
+++ StatTrackerLib/SqlConnector.cs
@@ -8,9 +8,16 @@ namespace StatTrackerLib
 {
     public class SqlConnector : IDataConnection
     {
+        // TODO - Make the Create prize method actually save to the database
+        /// <summary>
+        /// Saves a new prize to the database
+        /// </summary>
+        /// <param name="model">The prize information.</param>
+        /// <returns>The prize information including the prize identifier.</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            throw new NotImplementedException();
+            model.Id = 1;
+            return model;
         }
     }
 }
