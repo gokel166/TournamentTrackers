b380f76ba8ec4fb2638d7179d03dd59a3092c779
George Okello <okellogeorge40@yahoo.com>
2018-01-20 01:52:33 -0600

    Successfully create, save and append data on csv file

--- TrackerUI/Program.cs
+++ TrackerUI/Program.cs
@@ -19,7 +19,7 @@ namespace TrackerUI
             Application.SetCompatibleTextRenderingDefault(false);
 
             //Initialize the database connection
-            StatTrackerLib.Models.GlobalConfig.InitializeConnections(DatabaseType.Sql);
+            StatTrackerLib.Models.GlobalConfig.InitializeConnections(DatabaseType.TextFile);
 
             Application.Run(new CreatePrizeForm());
             //Application.Run(new TournamentDashboardForm());
